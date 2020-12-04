2ea9f9c6c06cd9574f68be08cd1fb74b745eea70
johnkgardner <johnkgardner2000@gmail.com>
2020-12-03 16:43:45 -0800

    caught up on day1

---
+++ 2020/day1.py
@@ -0,0 +1,18 @@
+# Read Input In
+data = open('input.txt', 'r').read()
+data = [int(n) for n in data.split()]
+
+
+#Part 1
+for i in range(len(data)): 
+   for j in range(i+1, len(data)): 
+      if data[i]+data[j] == 2020:
+         print(data[i]*data[j])
+
+
+# Part 2
+for i in range(len(data)):
+   for j in range(i+1, len(data)): 
+      for k in range(j+1, len(data)): 
+         if data[i] + data[j] + data[k] == 2020: 
+            print(data[i]*data[j]*data[k])
